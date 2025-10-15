6c69dcf23cfb33370b553ec087dc39ab94293cd5
simsekdogukan <doqusimsek@gmail.com>
2025-10-15 14:00:48 +0300

    Update getFiles.js

--- api/getFiles.js
+++ api/getFiles.js
@@ -1,27 +1,35 @@
 export default function handler(request, response) {
-    // Sadece POST isteklerini kabul et
-    if (request.method !== 'POST') {
-        return response.status(405).json({ error: 'Method Not Allowed' });
-    }
+    // Sadece POST isteklerini kabul et
+    if (request.method !== 'POST') {
+        return response.status(405).json({ error: 'Method Not Allowed' });
+    }
 
-    // Doğru şifre burada SAKLI
-    const correctPassword = '0000';
-    
-    // Google Drive linkleri burada GÜVENDE
-    const files = [
-        { name: "E-Sım kodları", url: "https://drive.google.com/file/d/1yuka5wfkXNW_d5VDPh27rcHycW5SYT14/view?usp=sharing" },
-        { name: "SolPro - Mayatech Bilgisayar Listesi", url: "https://docs.google.com/spreadsheets/d/1T4Myfgx8dBiG2WhJO4kf_YGTtO4t5kbp/edit?usp=drive_link&ouid=114707366481331827050&rtpof=true&sd=true" }
-    ];
+    // Doğru şifre burada SAKLI
+    const correctPassword = '0000';
+    
+    // Google Drive linkleri burada GÜVENDE
+    const files = [
+        { name: "Deneme 1", url: "#" },
+        { name: "Deneme 2", url: "#" },
+        { name: "Deneme 3", url: "#" },
+        { name: "Deneme 4", url: "#" },
+        { name: "Deneme 5", url: "#" },
+        { name: "Deneme 6", url: "#" },
+        { name: "Deneme 7", url: "#" },
+        { name: "Deneme 8", url: "#" },
+        { name: "Deneme 9", url: "#" },
+        { name: "Deneme 10", url: "#" }
+    ];
 
-    // Gelen şifreyi al
-    const { password } = request.body;
+    // Gelen şifreyi al
+    const { password } = request.body;
 
-    // Şifreyi kontrol et
-    if (password === correctPassword) {
-        // Şifre doğruysa, dosya listesini gönder
-        response.status(200).json(files);
-    } else {
-        // Şifre yanlışsa, yetkisiz hatası ver
-        response.status(401).json({ error: 'Invalid password' });
-    }
+    // Şifreyi kontrol et
+    if (password === correctPassword) {
+        // Şifre doğruysa, dosya listesini gönder
+        response.status(200).json(files);
+    } else {
+        // Şifre yanlışsa, yetkisiz hatası ver
+        response.status(401).json({ error: 'Invalid password' });
+    }
 }
